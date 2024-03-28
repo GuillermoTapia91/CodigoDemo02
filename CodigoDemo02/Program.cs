926090bad4291a35719a03358a22785e6a6e904b
GuillermoTapia91 <guillermo.tapia1991@gmail.com>
2024-03-27 22:17:54 -0500

    Agregar archivos de proyecto.

---
+++ CodigoDemo02/Program.cs
@@ -0,0 +1,206 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Uso If
+
+//int edad = 20;
+//if (edad>18)
+//{
+//    Console.WriteLine("Mayor de edad");
+//}
+
+// Solicitar dos números por consola e indicar por consola cual es el mayor usando IF ELSE
+//int numero1 = 0;
+//int numero2 = 0;
+
+//Console.WriteLine("Ingrese el primer número:");
+//numero1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Ingrese el segundo número:");
+//numero2 = Convert.ToInt32(Console.ReadLine());
+//if (numero1>numero2)
+//{
+//    Console.WriteLine("El primer número es mayor al segundo número");
+//}
+//else
+//{
+//    Console.WriteLine("El segundo número es mayor al primer número");
+//}
+
+
+//int calificacion = 0;
+
+//Console.WriteLine("Ingrese una calificación del 0 al 100:");
+//calificacion = Convert.ToInt32(Console.ReadLine());
+
+//if (calificacion >= 90)
+//{
+//    Console.WriteLine("Excelente");
+//}
+//if (calificacion>=89 && calificacion<=89)
+//{
+//    Console.WriteLine("Muy bueno");
+//}
+//if (calificacion >= 70 && calificacion <= 79)
+//{
+//    Console.WriteLine("Bueno");
+//}
+//if (calificacion >= 60 && calificacion <= 69)
+//{
+//    Console.WriteLine("Suficiente");
+//}
+//else
+//{
+//    Console.WriteLine("Insuficiente");
+//}
+
+//
+
+//int numeroSemana = 0;
+
+//Console.WriteLine("Ingrese un número de 1 al 7:");
+//numeroSemana = Convert.ToInt32(Console.ReadLine());
+
+//if (numeroSemana == 1)
+//{
+//    Console.WriteLine("Lunes");
+//}
+//if (numeroSemana == 2)
+//{
+//    Console.WriteLine("Martes");
+//}
+//if (numeroSemana == 3)
+//{
+//    Console.WriteLine("Miercoles");
+//}
+//if (numeroSemana == 4)
+//{
+//    Console.WriteLine("Jueves");
+//}
+//if (numeroSemana == 5)
+//{
+//    Console.WriteLine("Viernes");
+//}
+//if (numeroSemana == 6)
+//{
+//    Console.WriteLine("Sábado");
+//}
+//else
+//{
+//    Console.WriteLine("Domingo");
+//}
+
+
+
+//SOLICITAR UN NUMERO DEL 1 AL 12, Y SEGUN EL NÚMERO MOSTRAR EL MES EQUIVALENTE
+
+//int diaMes;
+//Console.WriteLine("Ingrese un número de 1 al 12:");
+//diaMes = Convert.ToInt32(Console.ReadLine());
+//string mes;
+
+//switch (diaMes)
+//{
+//    case 1:
+//        mes = "Enero";
+//    break;
+//    case 2:
+//        mes = "Febrero";
+//        break;
+//    case 3:
+//        mes = "Marzo";
+//        break;
+//    case 4:
+//        mes = "Abril";
+//        break;
+//    case 5:
+//        mes = "Mayo";
+//        break;
+//    case 6:
+//        mes = "Junio";
+//        break;
+//    case 7:
+//        mes = "Julio";
+//        break;
+//    case 8:
+//        mes = "Agosto";
+//        break;
+//    case 9:
+//        mes = "Septiembre";
+//        break;
+//    case 10:
+//        mes = "Octubre";
+//        break;
+//    case 11:
+//        mes = "Noviembre";
+//        break;
+//    case 12:
+//        mes = "Diciembre";
+//        break;
+//    default:
+//        mes = "Has ingresado un número fuera del rango del 1 al 12";
+//        break;
+//}
+//Console.WriteLine(mes);
+
+
+
+
+//int numeroMes;
+//Console.WriteLine("Ingrese un número de 1 al 12:");
+//numeroMes = Convert.ToInt32(Console.ReadLine());
+//string estacion;
+
+//switch (numeroMes)
+//{
+//    case 1:
+//        estacion = "Verano";
+//        break;
+//    case 2:
+//        estacion = "Verano";
+//        break;
+//    case 3:
+//        estacion = "Verano";
+//        break;
+//    case 4:
+//        estacion = "Otoño";
+//        break;
+//    case 5:
+//        estacion = "Otoño";
+//        break;
+//    case 6:
+//        estacion = "Otoño";
+//        break;
+//    case 7:
+//        estacion = "Primavera";
+//        break;
+//    case 8:
+//        estacion = "Primavera";
+//        break;
+//    case 9:
+//        estacion = "Primavera";
+//        break;
+//    case 10:
+//        estacion = "Invierno";
+//        break;
+//    case 11:
+//        estacion = "Invierno";
+//        break;
+//    case 12:
+//        estacion = "Invierno";
+//        break;
+//    default:
+//        estacion = "Has ingresado un número fuera del rango del 1 al 12";
+//        break;
+//}
+//Console.WriteLine(estacion);
+
+//Ejerciciio 10
+
+//int opcion;
+//Console.WriteLine("Ingrese una de las Opciones:");
+//Console.WriteLine("===========================:");
+//Console.WriteLine("/n");
+//Console.WriteLine("1.Calcular el área del cuadrado");
+//Console.WriteLine("2.Calcular el volumen del cubo");
+//Console.WriteLine("3.Salir");
+
+//opcion = C
